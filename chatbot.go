5e47cd604b5215fb3f9dc4c2ca66d6e5ae792c93
Kevin Barry <g00339811@gmit.ie>
2017-11-14 21:50:51 +0000

    restructured index page

--- chatbot.go
+++ chatbot.go
@@ -3,7 +3,7 @@
 *	Author	: Kevin Barry
 *	Date	: October 2017
 *	Data Represenation and Querying Project
-*	A chatbot web application using go based on ythe Eliza Program
+*	A chatbot web application using go based on the Eliza Program
 *
 *
 */
@@ -11,57 +11,27 @@
 package main
 
 import (
-	"html/template"//add html/template package 
 	"net/http"
-
+	"fmt"
+//	"strings"
 //	"bytes"
 )
 	
-type myMsg struct {
-	Input string
-	Output string
-	Previous string
-}
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	//serve the homepage.html file
-	http.ServeFile(w, r, "homepage.html")
-}
 
 func chatHandler(w http.ResponseWriter, r *http.Request) {
+	
 
-	//create and initialise string
-	output		:= "Howya lad"
-	input		:= r.FormValue("chat")
-	previous	:= input
-	
-//	// checking for chat URL encoded variable
-//	input, err := r.URL.Query()["guess"]
-//	// if not found execute the template and exit
-//	if !err || len(guess) < 1 {
-//		log.Println("Url Param 'guess' is missing")
-//		// execute the template with the message
-//		tmpl.Execute(w, m)
-//		return
-//	}// if
-	
-	// Query()["guess"] will return an array of items, 
-	// we only want the single item.
-//	g := guess[0]
-	
-		// adding the guess value to the user value
-	//read the contents of chat.html and return a template
-	t, _ := template.ParseFiles("chat.html")
-
-	//execute template and pass pointer to myMsg 	struct
-	t.Execute(w, &myMsg{Input:input,Output:output,Previous:previous})
 }//chatHandler
 
 func main() {
-	// handles root page
-	http.HandleFunc("/", requestHandler)
-
-	//handle /chat page
+	//serve the files from the /static folder
+	dir := http.Dir("./static")
+	fileServer := http.FileServer(dir)
+	//handle requests to /
+	http.Handle("/", fileServer)
+	//handle request to /chat
 	http.HandleFunc("/chat", chatHandler)
+
 	http.ListenAndServe(":8080", nil)
 }
